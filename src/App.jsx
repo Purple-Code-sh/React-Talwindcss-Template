1bd665fa31a8756403bdb55e7e17f72a222c8d9a
Purple-Code-sh <purple.code.sh@gmail.com>
2023-03-29 17:55:57 -0600

    First

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" w-60 h-32 bg-shPrimary-dark flex place-items-center justify-center text-white font-bold">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
